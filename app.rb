30b91c65f950144d50c955acaa8bfff5b880674f
povelko <povelkoea@gmail.com>
2021-03-19 16:32:10 +0600

    update++

--- app.rb
+++ app.rb
@@ -15,6 +15,22 @@ class Barber <ActiveRecord::Base
 
 end
 
+before do 
+	@barber = Barber.order "created_at desc"
+end
+
 get '/' do
+	
 	erb :index
+end
+get '/vizit' do
+	erb :vizit
+end
+post '/vizit' do
+	Client.create :name=> params[:username], :phone => params[:phone], :date => params[:date],
+	:color => params[:color], :barber => params[:barber]
+end
+post '/contacts' do
+	Client.create :name=> params[:username], :phone => params[:phone], :date => params[:date],
+	:color => params[:color], :barber => params[:barber]
 end
